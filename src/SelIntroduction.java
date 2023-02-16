f100a81c5456e1cf6b8d07c8fe2f0feb445ba001
SrinijaGurrala <srinija15g@gmail.com>
2023-02-16 10:56:18 +0530

    udated by srija

--- src/SelIntroduction.java
+++ src/SelIntroduction.java
@@ -6,16 +6,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class SelIntroduction {
 
 	public static void main(String[] args) {
-		//Invoking browser
-		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Srinija\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
-		//System.setProperty("webdriver.gecko.driver", "D:\\Testing documents\\geckodriver.exe");
 		
-		//System.setProperty("webdriver.edge.driver", "D:\\Testing documents\\msedgedriver.exe");
-
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.netmeds.com/");
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
+		System.out.println("GIT demo");
 		driver.close();
 	}
 
